da122473b93742ef9b216c8ce34c42e44acf106c
HrlCoder <634464975@qq.com>
2021-01-17 19:14:52 +0800

    String

---
+++ 长按键入/Solution.java
@@ -0,0 +1,22 @@
+package 字符串.长按键入;
+
+public class Solution {
+    public static boolean isLongPressedName(String name, String typed) {
+        int i = 0, j = 0;
+        while (j < typed.length()) {
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+                i++;
+                j++;
+            } else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
+                j++;
+            } else {
+                return false;
+            }
+        }
+        return i == name.length();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isLongPressedName("vtkgn", "vttkgnn"));
+    }
+}
