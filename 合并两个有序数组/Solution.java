6dd075ad384e9ea34c68e82c215663e17a2928f0
HrlCoder <634464975@qq.com>
2021-01-18 16:14:57 +0800

    数组

---
+++ 合并两个有序数组/Solution.java
@@ -0,0 +1,33 @@
+package 数组.合并两个有序数组;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        //从后向前插入
+        // l 表示从后向前当前插入位置
+        int l = m + n - 1;
+        // i 标记数组 nums1 当前比较位置
+        int i = m - 1;
+        // j 标记数组 nums2 当前比较位置
+        int j = n - 1;
+        //将数组 nums 2 全部数据插入到数组 nums1 为止
+        while (j >= 0 ) {
+            //当数组 nums1 当前可比较数据位 i 大于等于 0 并且数组 nums1 当前位置值比 nums2 当前位置值大时，将数组 nums1 当前位置数组 插入到 l 位置 ，i 位指针向前挪一位
+            //否则将 nums2 当前位置数据插入到 l 位置，j 位指针向前挪一位
+            // 当前插入位置 l 向前挪一位
+            nums1[l--] =  (i >= 0 && nums1[i] >= nums2[j]) ? nums1[l] = nums1[i--] : nums2[j--];
+        }
+    }
+
+    public static void merge1(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        merge(nums1,3,nums2,3);
+    }
+}
