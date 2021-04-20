fb55b96b182710ca9848445fe81f9a4442f51d7b
HrlCoder <634464975@qq.com>
2021-04-20 22:49:22 +0800

    动态规划

---
+++ 矩阵的最小路径和/Solution.java
@@ -0,0 +1,27 @@
+package 矩阵的最小路径和;
+
+public class Solution {
+    public int minPathSum (int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] dp = new int[n][m];
+        dp[0][0] = matrix[0][0];
+        for(int i = 1; i < m; i++) {
+            dp[0][i] = dp[0][i-1] + matrix[0][i];
+        }
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = dp[i-1][0] + matrix[i][0];
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 1;j < m; j++) {
+                dp[i][j] = Math.min(dp[i][j-1],dp[i-1][j]) + matrix[i][j];
+            }
+        }
+        return dp[n-1][m-1];
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
